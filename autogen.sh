f637f246efba180de89b3b808d64fe865ec48756
Jonas Juselius <jonas.juselius@uit.no>
2010-05-08 12:17:20 +0200

    Added autogen.sh.

---
+++ autogen.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+run ()
+{
+    echo "running: $*"
+    eval $*
+
+    if test $? != 0 ; then
+	echo "error: while running '$*'"
+	exit 1
+    fi
+}
+
+run aclocal -I config
+run autoheader
+run automake -a
+run autoconf
