986403e1f9b50210d6e45bb4fa336f08ddd31960
Roberto Di Remigio <robertodr@users.noreply.github.com>
2019-01-27 20:27:20 +0100

    Add alias for cleaner use with FetchContent (#29)

--- cmake/custom/tests.cmake
+++ cmake/custom/tests.cmake
@@ -1,3 +1,7 @@
-enable_testing()
-include(CTest)
-add_subdirectory(tests) # This must come last!!
+option_with_default(ENABLE_TESTS "Enable testing" ON)
+
+if(ENABLE_TESTS)
+  enable_testing()
+  include(CTest)
+  add_subdirectory(tests) # This must come last!!
+endif()
